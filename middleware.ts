ac1a6906bd4c224a86ddf82b443000cc59e6c481
supanile <supawit.nile@gmail.com>
2025-07-22 15:05:09 +0700

    refactor: streamline middleware logic by removing unnecessary comments and formatting

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,14 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(['/admin(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  // ใช้ clerkMiddleware เฉพาะเมื่อจำเป็น
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
-  
-  // สำหรับ route อื่นๆ ให้ผ่านไปเลย
-  return NextResponse.next();
-});
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
